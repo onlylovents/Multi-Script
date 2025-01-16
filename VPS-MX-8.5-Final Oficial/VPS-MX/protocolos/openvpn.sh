8a885e5d0efb81ee4321ac8dc4d5b1ea0ce16b29
onlylovents <onlyyounts@gmail.com>
2025-01-16 12:48:33 +0700

    Update openvpn.sh

--- VPS-MX-8.5-Final Oficial/VPS-MX/protocolos/openvpn.sh
+++ VPS-MX-8.5-Final Oficial/VPS-MX/protocolos/openvpn.sh
@@ -184,11 +184,11 @@ msg -bar
 		yum install openvpn iptables openssl ca-certificates -y
 	fi
 	# Get easy-rsa
-	EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz'
+	EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.2.1/EasyRSA-3.2.1.tgz'
 	wget -O ~/easyrsa.tgz "$EASYRSAURL" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$EASYRSAURL"
 	tar xzf ~/easyrsa.tgz -C ~/
-	mv ~/EasyRSA-3.0.8/ /etc/openvpn/
-	mv /etc/openvpn/EasyRSA-3.0.8/ /etc/openvpn/easy-rsa/
+	mv ~/EasyRSA-3.2.1/ /etc/openvpn/
+	mv /etc/openvpn/EasyRSA-3.2.1/ /etc/openvpn/easy-rsa/
 	chown -R root:root /etc/openvpn/easy-rsa/
 	rm -f ~/easyrsa.tgz
 	cd /etc/openvpn/easy-rsa/
